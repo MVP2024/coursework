a94d2bb3b3dff5fa28d1e60006ac0a8e46779463
Valery Medvedev <walery.medvedev@gmail.com>
2025-02-15 08:58:52 +0300

    функции для "Главной"

--- src/categories.py
+++
@@ -1,5 +0,0 @@
-
-# def get_date_range(start_date, end_date):
-#     """Возвращает список дат в диапазоне от start_date до end_date."""
-#     delta = end_date - start_date
-#     return [start_date + timedelta(days=i) for i in range(delta.days + 1)]
